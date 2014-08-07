54d6125d90f7c5809e443dd0393acac7e3028764
HexagonStorms <josueplazamusic@gmail.com>
2014-08-07 13:41:11 -0500

    Added boilerplate and Google analytics

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="author" content="Josue Plaza">
 	<meta property="og:title" content="Covo">
 	<meta property="og:type" content="website">
@@ -15,6 +15,10 @@
 
     <title>@yield('title')</title>
 
+    <!-- H5BP CSS -->
+    <link rel="stylesheet" href="assets/css/normalize.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+
     <!-- Bootstrap CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
 
@@ -25,8 +29,8 @@
     <!-- Josue Plaza's custom styles -->
     <link href="assets/css/josuestyle.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!-- Modernizr -->
+    <script src="assets/js/modernizr-2.6.2.min.js"></script>
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -85,11 +89,23 @@
 			</div>
 		</div>
 	</div>
-	
-
+	 
+    <!-- H5BP JS -->
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/main.js"></script>
 
     <!-- Bootstrap  JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Google Analytics (empty) -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
   </body>
 </html>
