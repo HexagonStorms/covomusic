48b0c535cdd3bdf2979277ad1bb0f1b82dd6126e
Josue Plaza <josueplazamusic@gmail.com>
2014-10-13 21:31:56 -0500

    Attempting to fix about me off centering

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -59,7 +59,7 @@ Covo Music | Home
 		<div class="centered">
 			<h1>About Me</h1>
 		</div>
-		<div class="col-lg-3 col-md-6 col-sm-12">
+		<div class="col-sm-offset-1 col-lg-3 col-md-6 col-sm-12">
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -75,14 +75,7 @@ Covo Music | Home
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
-		<div class="col-lg-3 col-md-6 col-sm-12">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		</div>
+		
 	</div>
 </div>
 
